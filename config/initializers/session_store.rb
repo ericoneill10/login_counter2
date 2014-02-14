fa8e563fa5c248a74dffcabba88309afdfa27b31
Eric O'Neill <ericoneill10@gmail.com>
2014-02-14 15:11:48 -0800

    possibly fixed heroku deployment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-LoginCounter2::Application.config.session_store :cookie_store, key: '_login_counter2_session'
+LoginCounter2::Application.config.session_store :cookie_store, key: '_login_counter2_session', domain: :all
